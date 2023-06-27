50d0c8704bf4272a7f4bd74ae66b6030b7ea4f94
Shafiya Naaz <shafiya.naaz@lybl.com>
2023-06-27 17:24:07 +0530

    changes to model

--- models/test_dbt.sql
+++ models/test_dbt.sql
@@ -1,6 +1,6 @@
 
 
-create table "basic-profile".basic_profile."userProfileFinal" compound sortkey(_airbyte_emitted_at)
+create table basic_profile."userProfileFinal" compound sortkey(_airbyte_emitted_at)
 
 as (
 
@@ -23,5 +23,5 @@ select
         _airbyte_ab_id,
     	_airbyte_emitted_at,
     	getdate() as _airbyte_normalized_at
-from "basic-profile".basic_profile."_airbyte_raw_userprofile"
+from basic_profile."_airbyte_raw_userprofile"
   );
