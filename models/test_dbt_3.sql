0b94084af268e638f6d82f217e46058bf4c50673
Shafiya Naaz <shafiya.naaz@lybl.com>
2023-06-28 15:13:11 +0530

    test 3

--- models/test_dbt_3.sql
+++ models/test_dbt_3.sql
@@ -1,16 +1,11 @@
--- process_array.sql
-
--- Step 1: Unnest the array into individual rows
 WITH unnested_data AS (
   SELECT
     id,
     unnest(profileList) AS array_item
   FROM
     "basic-profile".basic_profile."_airbyte_raw_userprofile"
-    -- {{ source('my_source', 'my_table') }}
 ),
 
--- Step 2: Extract properties from the objects in the array
 extracted_data AS (
   SELECT
     id,
@@ -18,22 +13,6 @@ extracted_data AS (
     array_item ->> 'name' AS name
   FROM
     unnested_data
-),
-
--- -- Step 3: Apply further transformations or aggregations as needed
--- transformed_data AS (
---   SELECT
---     id,
---     object_id,
---     UPPER(object_name) AS capitalized_name,
---     CASE
---       WHEN object_age > 18 THEN 'Adult'
---       ELSE 'Minor'
---     END AS age_group
---   FROM
---     extracted_data
--- )
-
--- Step 4: Output the final transformed data
+)
 SELECT *
 FROM extracted_data
