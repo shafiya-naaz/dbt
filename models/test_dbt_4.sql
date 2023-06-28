c04040a8c9cc963f899a08d772d802b4a667ec3c
Shafiya Naaz <shafiya.naaz@lybl.com>
2023-06-28 17:06:18 +0530

    test 4

---
+++ models/test_dbt_4.sql
@@ -0,0 +1,9 @@
+with parsed_data as (
+    select 
+    json_parse(json_serialize(_airbyte_data)) as new_data 
+    from "basic-profile".basic_profile."_airbyte_raw_userprofile"
+),
+extracted_data as (
+	select json_extract_path_text(new_data, 'profileList', true) as profiles from parsed_data
+)
+select * from extracted_data;
